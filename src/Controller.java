b21324129393d7f6f3d671ca1d3a12ae0363924d
Szadam <sziver.adi@gmail.com>
2022-05-11 13:05:50 +0200

    ver 2.1 - Controller beta

---
+++ src/Controller.java
@@ -0,0 +1,85 @@
+import static java.lang.String.valueOf;
+import static java.lang.Double.valueOf;
+import java.util.HashMap;
+
+public class Controller {
+    boolean kell=false;
+    HashMap<Double, Double> tem_hum = new HashMap<Double, Double>();
+
+    public class TooHighTempDiff extends Exception {
+        public double difference;
+        public TooHighTempDiff(double difference) {
+            super("Too high difference between optimal and actual temperature: "+String.valueOf(difference));
+            this.difference=difference;
+        }
+    }
+    public class TooHighHumidityDiff extends Exception {
+        public double difference;
+        public TooHighHumidityDiff(double difference) {
+            super("Too high difference between optimal and actual Humidity: "+String.valueOf(difference));
+            this.difference=difference;
+        }
+    }
+
+    String kazan(double temperature_act, double humidity_act, double temperature_min, double humidity_min, double temperature_opt, boolean boiler_on, boolean sprinkler_on) throws TooHighTempDiff, TooHighHumidityDiff{
+        if((temperature_opt-temperature_act)>5)
+            throw new TooHighTempDiff((temperature_opt-temperature_act));
+        if((humidity_min-humidity_act)>20)
+            throw new TooHighHumidityDiff((humidity_min-humidity_act));
+        tem_hum.put(Double.valueOf(20),17.3);
+        tem_hum.put(Double.valueOf(21),18.5);
+        tem_hum.put(Double.valueOf(22),19.7);
+        tem_hum.put(Double.valueOf(23),20.9);
+        tem_hum.put(Double.valueOf(24),22.1);
+        tem_hum.put(Double.valueOf(25),23.2);
+        tem_hum.put(Double.valueOf(26),24.7);
+        tem_hum.put(Double.valueOf(27),26.1);
+        tem_hum.put(Double.valueOf(28),27.5);
+        tem_hum.put(Double.valueOf(29),28.9);
+        tem_hum.put(Double.valueOf(30),30.3);
+        tem_hum.put(Double.valueOf(21),31.9);
+        tem_hum.put(Double.valueOf(32),33.5);
+        tem_hum.put(Double.valueOf(33),35.1);
+        tem_hum.put(Double.valueOf(34),36.7);
+        tem_hum.put(Double.valueOf(35),38.3);
+        if(!boiler_on && !sprinkler_on){
+            if(temperature_act>temperature_min && humidity_act>humidity_min){
+                return "";
+            }else{
+                kell=true;
+                return "bup"+String.valueOf((int)(temperature_opt-temperature_act))+"c";
+            }
+        }else{
+            return "";
+        }
+    }
+    String locsolo(double greenhouse_size, double temperature_act, double humidity_act, double humidity_min, double temperature_opt){
+        if(kell){
+            double plus_water_necessary=0;
+            double gm3_act = 0; //25 esetén 23,3 |25.0
+            double gm3_opt = 0; //30 esetén 30,3 |30
+            for (Double i : tem_hum.keySet()) {
+                if(i==temperature_act)
+                    gm3_act=tem_hum.get(i);
+            }
+            for (Double i : tem_hum.keySet()) {
+                if(i==temperature_opt)
+                    gm3_opt=tem_hum.get(i);
+            }
+            double gm3_hum_act = gm3_act * (humidity_act/100);//23,3*0,7 = 16,31 |13.98
+            double fut_gm3_hum = gm3_hum_act / gm3_opt; //16,31/30,3 |0.46138613861
+            if(humidity_min>fut_gm3_hum) //60>53,82
+            {
+                double fut_hum_min = gm3_opt * (humidity_min/100); // 18,18
+                double plus_water = fut_hum_min - gm3_hum_act; // 1,87 |4.2
+                plus_water /= 0.01; //1%os párologtatás
+                plus_water *= greenhouse_size;
+                plus_water /=1000;
+                plus_water_necessary = plus_water;
+            }
+            return "son"+String.valueOf((int)plus_water_necessary)+"l";
+        }else{
+            return "";
+        }
+    }
+}
--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.*;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import static java.lang.Integer.parseInt;
 
@@ -9,15 +11,33 @@ public class Driver implements  IDriver {
         URL url;
         String controlToken;
         url = new URL("http://193.6.19.58:8181/greenhouse/" + token);
-
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("POST");
         con.setRequestProperty("Content-Type", "text/plain");
         con.setRequestProperty("charset", "utf-8");
         con.setDoOutput(true);
+        IMonitor.SensorData currentSensorData = new Monitor().getSensorData(gh.getGhId());
+        Controller currentCommand = new Controller();
+        String bC="";
+        try {
+            bC = currentCommand.kazan(currentSensorData.getTemperature_act(), currentSensorData.getHumidity_act(), gh.getTemperature_min(), gh.getHumidity_min(), gh.getTemperature_opt(), currentSensorData.isBoiler_on(), currentSensorData.isSprinkler_on());
+            System.out.println(bC);
 
-        String jsonInputString = "{\"ghId\":\"KFI3EW45RD\",\"boilerCommand\":\"bup5c\",\"sprinklerCommand\":\"son224l\"}";
-
+        }
+        catch (Controller.TooHighHumidityDiff e){
+            FileWriter myWriter = new FileWriter("error_log.txt");
+            myWriter.write(String.valueOf(LocalDateTime.now()) + " " + String.valueOf(LocalTime.now()) + " Too high difference between optimal and actual temperature:" + String.valueOf(e.difference));
+            myWriter.close();
+        }
+        catch (Controller.TooHighTempDiff e){
+            FileWriter myWriter = new FileWriter("error_log.txt");
+            myWriter.write(String.valueOf(LocalDateTime.now()) + " " + String.valueOf(LocalTime.now()) + " Too high difference between optimal and actual humidity:" + String.valueOf(e.difference));
+            myWriter.close();
+        }
+        String sC = currentCommand.locsolo(Double.valueOf(gh.getVolume()),currentSensorData.getTemperature_act(),currentSensorData.getHumidity_act(),gh.getHumidity_min(),gh.getTemperature_opt());
+        System.out.println(sC);
+        String jsonInputString = "{\"ghId\":\""+gh.getGhId()+"\",\"boilerCommand\":\""+bC+"\",\"sprinklerCommand\":\""+sC+"\"}";
+        System.out.println(jsonInputString);
 
         try(OutputStream os = con.getOutputStream()) {
             byte[] input = jsonInputString.getBytes("utf-8");
